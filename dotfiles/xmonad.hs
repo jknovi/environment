74b3383b5117627d1db8c0774064c2b26414421b
Javier Cano <javier@ocelot.net>
2023-03-19 12:39:32 -0700

    Updated to new setup

--- dotfiles/xmonad.hs
+++ dotfiles/xmonad.hs
@@ -5,22 +5,30 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Util.SpawnOnce
+import XMonad.Layout.IndependentScreens
 
 myStartupHook :: X ()
 myStartupHook = do
-    spawnOnce "picom"
-    spawnOnce "feh --randomize --bg-fill ~/wallpapers/*"
+    { screens <- countScreens
+    ; case screens of
+       1 -> do
+         spawn "xrandr --output eDP-1 --primary --scale .75x.75"
+       3 -> do
+         spawn "xrandr --output eDP-1 --left-of DP-4 --scale .75x.75 --output DP-4  --primary --left-of DP-3"
+       _ -> return ()
+    ; spawnOnce "picom"
+    ; spawnOnce "feh --randomize --bg-fill ~/wallpapers/*"
+    }
 
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ docks defaultConfig
-        { layoutHook = avoidStruts $ layoutHook defaultConfig
+    xmonad $ docks def
+        { layoutHook = avoidStruts $ layoutHook def
         , logHook = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc
             , ppTitle = xmobarColor "green" "" . shorten 50
             }
-        , modMask     = mod4Mask  -- Rebind mod to the windows key
         , terminal    = "alacritty"
         , startupHook = myStartupHook
         }
